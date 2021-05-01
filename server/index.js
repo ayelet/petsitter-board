78eeb9397045c98ecb2eaa41d402b14a769d09cb
Ayelet Danieli <ayeletd@gmail.com>
2021-05-01 05:26:27 +0300

    server and mongo up

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000;
+const cors = require("cors");
+require("./DB/mongoose");
+// const newRoute = require("./routes/newRoutes.routes");
+
+app.use(express.json());
+app.use(cors());
+// app.use(newRoute);
+
+app.listen(port, () => {
+  console.log(`server run at http://localhost:${port}`);
+});
