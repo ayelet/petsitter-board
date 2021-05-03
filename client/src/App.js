72e8e81d13b890a603a0e3894ba5128fe41f42a5
Ayelet Danieli <ayeletd@gmail.com>
2021-05-03 16:02:21 +0300

    welcome page change

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import welcome from "./assets/img/welcome.jpg";
 function App() {
   return (
     <div className="App">
-      <h1>Comming Soon</h1>
+      <h1>Coming Soon</h1>
       <img src={welcome} alt="welcome"></img>
     </div>
   );
