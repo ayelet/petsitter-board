c2edb96179053701f00157750c2f1e98e8ec554d
Ayelet Danieli <ayeletd@gmail.com>
2021-05-03 12:43:28 +0300

    fixes (?) heroku production bug

--- server/index.js
+++ server/index.js
@@ -12,17 +12,13 @@ app.use(express.json());
 app.use(cors());
 app.use(newRoute);
 
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  // app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) => {
     res.sendfile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
-
-
-
 
 app.listen(port, () => {
   console.log(`### server run at ${port}`);
